788727c36468efec660b30666a77e13ba8641718
Tech-sis <abilawonadeola@gmail.com>
2022-08-25 22:22:31 -0400

    Measure height and depth of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,10 +12,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	if (left > right)
-		return (left + 1);
+		return (left);
 	else
-		return (right + 1);
+		return (right);
 }
